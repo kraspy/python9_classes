ee0694faabb92636fb1d4316206430e789495cef
Evgeniy Doroshenko <kraspy@yandex.ru>
2024-11-17 17:58:40 +0700

    Initial commit

---
+++ classes/shoping_carts.py
@@ -0,0 +1,37 @@
+# 3. Класс для управления корзиной покупок
+
+class ShoppingCart:
+    """
+    Класс, представляющий корзину покупок.
+    """
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, product, quantity):
+        """
+        Добавляет продукт в корзину.
+        """
+        self.items.append({"Продукт": product, "количество": quantity})
+
+    def remove_item(self, product_name):
+        """
+        Удаляет продукт из корзины по имени.
+        """
+        self.items = [item for item in self.items if item["Продукт"].name != product_name]
+
+    def get_total(self):
+        """
+        Возвращает общую стоимость продуктов в корзине.
+        """
+        total = sum(item["Продукт"].price * item["количество"] for item in self.items)
+        return total
+
+    def get_details(self):
+        """
+        Возвращает детализированную информацию о содержимом корзины и общей стоимости.
+        """
+        details = "Корзина покупок:\n"
+        for item in self.items:
+            details += f"{item['Продукт'].get_details()}, Количество: {item['количество']}\n"
+        details += f"Общее: {self.get_total()} руб"
+        return details
